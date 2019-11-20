00d0cbb5a507682f2f57121f00745858a891504d
Matthew Weis <matthewweiscaps@gmail.com>
2019-11-20 17:47:03 -0600

    reusing bgfx4j components

--- src/test/java/com/bariumhoof/bgfx4j/examples/sanity_check/SanityCheck.java
+++ src/test/java/com/bariumhoof/bgfx4j/examples/sanity_check/SanityCheck.java
@@ -50,17 +50,10 @@ public class SanityCheck extends Application {
             6, 3, 7
     };
 
-    //        private BGFXVertexLayout layout;
-//        private VertexDecl layout;
-    private BGFXVertexDecl layout;
-    private ByteBuffer vertices;
-    //        private VertexBuffer vertices;
-    private short vbh;
+    private VertexDecl layout;
+    private VertexBuffer vertices;
 
-    private ByteBuffer indices;
-    //        private IndexBuffer indices;
-    private short ibh;
-    //        private short program;
+    private IndexBuffer indices;
     private Program program;
     private Matrix4f view = new Matrix4f();
     private FloatBuffer viewBuf;
@@ -75,29 +68,13 @@ public class SanityCheck extends Application {
     @Override
     public void init() {
 
-//        layout = VertexDecl.builder(BGFX_RENDERER_TYPE.METAL)
-//                .beginWith(BGFX_ATTRIB.POSITION, BGFX_ATTRIB_TYPE.FLOAT, false, false)
-//                .thenUse(BGFX_ATTRIB.COLOR0, BGFX_ATTRIB_TYPE.UINT8, true, false)
-//                .build();
+        layout = VertexDecl.builder(BGFX_RENDERER_TYPE.METAL)
+                .beginWith(BGFX_ATTRIB.POSITION, BGFX_ATTRIB_TYPE.FLOAT, false, false)
+                .thenUse(BGFX_ATTRIB.COLOR0, BGFX_ATTRIB_TYPE.UINT8, true, false)
+                .build();
 
-        layout = createVertexLayout(    false, true, 0);
-
-//        vertices = MemoryUtil.memAlloc(8 * (3 * 4 + 4));
-
-        vertices = MemoryUtil.memAlloc(8 * (3 * 4 + 4));
-
-        vbh = createVertexBuffer(vertices, layout, cubeVertices);
-//        vertices = VertexBuffer.create(layout, cubeVertices);
-//        vbh = vertices.handle();
-
-        indices = MemoryUtil.memAlloc(cubeIndices.length * 2);
-        ibh = createIndexBuffer(indices, cubeIndices);
-//        indices = IndexBuffer.create(cubeIndices);
-//        ibh = indices.handle();
-
-
-//        short vs = BGFXDemoUtil.loadShader("vs_cubes");
-//        short fs = BGFXDemoUtil.loadShader("fs_cubes");
+        vertices = VertexBuffer.create(layout, cubeVertices);
+        indices = IndexBuffer.create(cubeIndices);
 
         program = Program.loadOrNull(
                 SanityCheck.class.getResource("/shaders/metal/cubes.vert"), // vertex shader
@@ -107,8 +84,6 @@ public class SanityCheck extends Application {
         viewBuf = MemoryUtil.memAllocFloat(16);
         projBuf = MemoryUtil.memAllocFloat(16);
         modelBuf = MemoryUtil.memAllocFloat(16);
-
-
     }
 
     @Override
@@ -117,8 +92,6 @@ public class SanityCheck extends Application {
         bgfx_dbg_text_printf(0, 1, 0x4f, "bgfx/examples/01-cubes");
         bgfx_dbg_text_printf(0, 2, 0x6f, "Description: Rendering simple static mesh.");
 
-//        BGFXDemoUtil.lookAt(new Vector3f(0.0f, 0.0f, 0.0f), new Vector3f(0.0f, 0.0f, -35.0f), view);
-//        BGFXDemoUtil.perspective(60.0f, getWidth(), getHeight(), 0.1f, 100.0f, proj);
         lookAt(new Vector3f(0.0f, 0.0f, 0.0f), new Vector3f(0.0f, 0.0f, -35.0f), view);
         perspective(60.0f, getWidth(), getHeight(), 0.1f, 100.0f, proj);
 
@@ -138,11 +111,9 @@ public class SanityCheck extends Application {
                                         0.0f)
                                 .get(modelBuf));
 
-                bgfx_set_vertex_buffer(0, vbh, 0, 8);
-                bgfx_set_index_buffer(ibh, 0, 36);
-
+                bgfx_set_vertex_buffer(0, vertices.handle(), 0, 8);
+                bgfx_set_index_buffer(indices.handle(), 0, 36);
                 bgfx_set_state( BGFX_STATE_DEFAULT, 0);
-
                 bgfx_submit(0, program.handle(), 0, false);
             }
         }
@@ -151,20 +122,17 @@ public class SanityCheck extends Application {
 
     @Override
     public void dispose() {
-//        if(m_ibh!=null){
-//        m_ibh.dispose();
-//        }
 
         MemoryUtil.memFree(viewBuf);
         MemoryUtil.memFree(projBuf);
         MemoryUtil.memFree(modelBuf);
 
         if (vertices != null) {
-//                vertices.dispose();
+            vertices.dispose();
         }
 
         if (indices != null) {
-//                indices.dispose();
+            indices.dispose();
         }
 
         if (program != null) {
@@ -174,98 +142,6 @@ public class SanityCheck extends Application {
 
     public static void main(String[] args) throws IOException {
         new SanityCheck().start();
-    }
-
-    static BGFXVertexDecl createVertexLayout(boolean withNormals, boolean withColor, int numUVs) {
-
-        BGFXVertexDecl layout = BGFXVertexDecl.calloc();
-
-        bgfx_vertex_decl_begin(layout, BGFX_RENDERER_TYPE.METAL.VALUE);
-
-        bgfx_vertex_decl_add(layout,
-                BGFX_ATTRIB_POSITION,
-                3,
-                BGFX_ATTRIB_TYPE_FLOAT,
-                false,
-                false);
-
-        if (withNormals) {
-            bgfx_vertex_decl_add(layout,
-                    BGFX_ATTRIB_NORMAL,
-                    3,
-                    BGFX_ATTRIB_TYPE_FLOAT,
-                    false,
-                    false);
-        }
-
-        if (withColor) {
-            bgfx_vertex_decl_add(layout,
-                    BGFX_ATTRIB_COLOR0,
-                    4,
-                    BGFX_ATTRIB_TYPE_UINT8,
-                    true,
-                    false);
-        }
-
-        if (numUVs > 0) {
-            bgfx_vertex_decl_add(layout,
-                    BGFX_ATTRIB_TEXCOORD0,
-                    2,
-                    BGFX_ATTRIB_TYPE_FLOAT,
-                    false,
-                    false);
-        }
-
-        bgfx_vertex_decl_end(layout);
-
-        return layout;
-    }
-
-    static short createVertexBuffer(ByteBuffer buffer, BGFXVertexDecl layout, Object[][] vertices) {
-
-        for (Object[] vtx : vertices) {
-            for (Object attr : vtx) {
-                if (attr instanceof Float) {
-                    buffer.putFloat((float) attr);
-                } else if (attr instanceof Integer) {
-                    buffer.putInt((int) attr);
-                } else {
-                    throw new RuntimeException("Invalid parameter type");
-                }
-            }
-        }
-
-        if (buffer.remaining() != 0) {
-            throw new RuntimeException("ByteBuffer size and number of arguments do not match");
-        }
-
-        buffer.flip();
-
-        return createVertexBuffer(buffer, layout);
-    }
-
-    static short createVertexBuffer(ByteBuffer buffer, BGFXVertexDecl layout) {
-
-        BGFXMemory vbhMem = bgfx_make_ref(buffer);
-
-        return bgfx_create_vertex_buffer(vbhMem, layout, BGFX_BUFFER_NONE);
-    }
-
-    static short createIndexBuffer(ByteBuffer buffer, int[] indices) {
-
-        for (int idx : indices) {
-            buffer.putShort((short) idx);
-        }
-
-        if (buffer.remaining() != 0) {
-            throw new RuntimeException("ByteBuffer size and number of arguments do not match");
-        }
-
-        buffer.flip();
-
-        BGFXMemory ibhMem = bgfx_make_ref(buffer);
-
-        return bgfx_create_index_buffer(ibhMem, BGFX_BUFFER_NONE);
     }
 
 }
